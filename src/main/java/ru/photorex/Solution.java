04ede5006610a79e636f6e5595bb65e39cb5dadc
Dima Merechko <mfocus@mail.ru>
2019-02-06 16:35:03 +0200

    add Util class with telBook init

---
+++ src/main/java/ru/photorex/Solution.java
@@ -0,0 +1,10 @@
+package ru.photorex;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/ru/photorex/Util.java
@@ -0,0 +1,28 @@
+package ru.photorex;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Util {
+
+    public Map getBook(){
+        List<String> ivanov = new ArrayList<String>();
+        ivanov.add("+8 800 2000 500");
+        ivanov.add("+8 800 200 600");
+        List <String> petrov = new ArrayList<String>();
+        petrov.add("+8 800 2000 700");
+        List <String> sidorov = new ArrayList<String>();
+        sidorov.add("+8 800 2000 800");
+        sidorov.add("+8 800 2000 900");
+        sidorov.add("+8 800 2000 000");
+
+        Map<String, List<String>> book = new HashMap<String, List<String>>();
+        book.put("Иванов И.И.", ivanov);
+        book.put("Петров П.П.", petrov);
+        book.put("Сидоров С.С.", sidorov);
+
+        return book;
+    }
+}
